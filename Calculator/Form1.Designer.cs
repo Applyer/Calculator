b79acc19c6d202f68087fbe6a7030f24310ed511
evoltech-khallouf <khallouf-a@evoltech.co.jp>
2019-09-10 10:42:31 +0900

    初期段階

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.n1 = new System.Windows.Forms.Button();
+            this.n2 = new System.Windows.Forms.Button();
+            this.n3 = new System.Windows.Forms.Button();
+            this.n4 = new System.Windows.Forms.Button();
+            this.n5 = new System.Windows.Forms.Button();
+            this.n6 = new System.Windows.Forms.Button();
+            this.n7 = new System.Windows.Forms.Button();
+            this.n8 = new System.Windows.Forms.Button();
+            this.n9 = new System.Windows.Forms.Button();
+            this.n0 = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.sub = new System.Windows.Forms.Button();
+            this.mul = new System.Windows.Forms.Button();
+            this.div = new System.Windows.Forms.Button();
+            this.dot = new System.Windows.Forms.Button();
+            this.c = new System.Windows.Forms.Button();
+            this.equal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(534, 42);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // n1
+            // 
+            this.n1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.n1.Font = new System.Drawing.Font("MS UI Gothic", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.n1.Location = new System.Drawing.Point(46, 267);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(75, 75);
+            this.n1.TabIndex = 1;
+            this.n1.Text = "1";
+            this.n1.UseVisualStyleBackColor = false;
+            this.n1.Click += new System.EventHandler(this.n1_Click);
+            // 
+            // n2
+            // 
+            this.n2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.n2.Font = new System.Drawing.Font("MS UI Gothic", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.n2.Location = new System.Drawing.Point(140, 267);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(75, 75);
+            this.n2.TabIndex = 2;
+            this.n2.Text = "2";
+            this.n2.UseVisualStyleBackColor = false;
+            this.n2.Click += new System.EventHandler(this.n2_Click);
+            // 
+            // n3
+            // 
+            this.n3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.n3.Font = new System.Drawing.Font("MS UI Gothic", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.n3.Location = new System.Drawing.Point(236, 267);
+            this.n3.Name = "n3";
+            this.n3.Size = new System.Drawing.Size(75, 75);
+            this.n3.TabIndex = 3;
+            this.n3.Text = "3";
+            this.n3.UseVisualStyleBackColor = false;
+            this.n3.Click += new System.EventHandler(this.n3_Click);
+            // 
+            // n4
+            // 
+            this.n4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.n4.Font = new System.Drawing.Font("MS UI Gothic", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.n4.Location = new System.Drawing.Point(46, 175);
+            this.n4.Name = "n4";
+            this.n4.Size = new System.Drawing.Size(75, 75);
+            this.n4.TabIndex = 4;
+            this.n4.Text = "4";
+            this.n4.UseVisualStyleBackColor = false;
+            this.n4.Click += new System.EventHandler(this.n4_Click);
+            // 
+            // n5
+            // 
+            this.n5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.n5.Font = new System.Drawing.Font("MS UI Gothic", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.n5.Location = new System.Drawing.Point(140, 175);
+            this.n5.Name = "n5";
+            this.n5.Size = new System.Drawing.Size(75, 75);
+            this.n5.TabIndex = 5;
+            this.n5.Text = "5";
+            this.n5.UseVisualStyleBackColor = false;
+            this.n5.Click += new System.EventHandler(this.n5_Click);
+            // 
+            // n6
+            // 
+            this.n6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.n6.Font = new System.Drawing.Font("MS UI Gothic", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.n6.Location = new System.Drawing.Point(236, 175);
+            this.n6.Name = "n6";
+            this.n6.Size = new System.Drawing.Size(75, 75);
+            this.n6.TabIndex = 6;
+            this.n6.Text = "6";
+            this.n6.UseVisualStyleBackColor = false;
+            this.n6.Click += new System.EventHandler(this.n6_Click);
+            // 
+            // n7
+            // 
+            this.n7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.n7.Font = new System.Drawing.Font("MS UI Gothic", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.n7.Location = new System.Drawing.Point(46, 84);
+            this.n7.Name = "n7";
+            this.n7.Size = new System.Drawing.Size(75, 75);
+            this.n7.TabIndex = 7;
+            this.n7.Text = "7";
+            this.n7.UseVisualStyleBackColor = false;
+            this.n7.Click += new System.EventHandler(this.n7_Click);
+            // 
+            // n8
+            // 
+            this.n8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.n8.Font = new System.Drawing.Font("MS UI Gothic", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.n8.Location = new System.Drawing.Point(140, 84);
+            this.n8.Name = "n8";
+            this.n8.Size = new System.Drawing.Size(75, 75);
+            this.n8.TabIndex = 8;
+            this.n8.Text = "8";
+            this.n8.UseVisualStyleBackColor = false;
+            this.n8.Click += new System.EventHandler(this.n8_Click);
+            // 
+            // n9
+            // 
+            this.n9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.n9.Font = new System.Drawing.Font("MS UI Gothic", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.n9.Location = new System.Drawing.Point(236, 84);
+            this.n9.Name = "n9";
+            this.n9.Size = new System.Drawing.Size(75, 75);
+            this.n9.TabIndex = 9;
+            this.n9.Text = "9";
+            this.n9.UseVisualStyleBackColor = false;
+            this.n9.Click += new System.EventHandler(this.n9_Click);
+            // 
+            // n0
+            // 
+            this.n0.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.n0.Font = new System.Drawing.Font("MS UI Gothic", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.n0.Location = new System.Drawing.Point(140, 359);
+            this.n0.Name = "n0";
+            this.n0.Size = new System.Drawing.Size(75, 75);
+            this.n0.TabIndex = 10;
+            this.n0.Text = "0";
+            this.n0.UseVisualStyleBackColor = false;
+            this.n0.Click += new System.EventHandler(this.n0_Click);
+            // 
+            // add
+            // 
+            this.add.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.add.Font = new System.Drawing.Font("MS UI Gothic", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.add.Location = new System.Drawing.Point(334, 84);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 75);
+            this.add.TabIndex = 11;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = false;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // sub
+            // 
+            this.sub.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.sub.Font = new System.Drawing.Font("MS UI Gothic", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.sub.Location = new System.Drawing.Point(334, 175);
+            this.sub.Name = "sub";
+            this.sub.Size = new System.Drawing.Size(75, 75);
+            this.sub.TabIndex = 12;
+            this.sub.Text = "-";
+            this.sub.UseVisualStyleBackColor = false;
+            this.sub.Click += new System.EventHandler(this.sub_Click);
+            // 
+            // mul
+            // 
+            this.mul.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.mul.Font = new System.Drawing.Font("MS UI Gothic", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.mul.Location = new System.Drawing.Point(334, 267);
+            this.mul.Name = "mul";
+            this.mul.Size = new System.Drawing.Size(75, 75);
+            this.mul.TabIndex = 13;
+            this.mul.Text = "*";
+            this.mul.UseVisualStyleBackColor = false;
+            this.mul.Click += new System.EventHandler(this.mul_Click);
+            // 
+            // div
+            // 
+            this.div.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.div.Font = new System.Drawing.Font("MS UI Gothic", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.div.Location = new System.Drawing.Point(334, 359);
+            this.div.Name = "div";
+            this.div.Size = new System.Drawing.Size(75, 75);
+            this.div.TabIndex = 14;
+            this.div.Text = "/";
+            this.div.UseVisualStyleBackColor = false;
+            this.div.Click += new System.EventHandler(this.div_Click);
+            // 
+            // dot
+            // 
+            this.dot.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.dot.Font = new System.Drawing.Font("MS UI Gothic", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.dot.Location = new System.Drawing.Point(236, 359);
+            this.dot.Name = "dot";
+            this.dot.Size = new System.Drawing.Size(75, 75);
+            this.dot.TabIndex = 15;
+            this.dot.Text = ".";
+            this.dot.UseVisualStyleBackColor = false;
+            this.dot.Click += new System.EventHandler(this.dot_Click);
+            // 
+            // c
+            // 
+            this.c.BackColor = System.Drawing.Color.White;
+            this.c.Font = new System.Drawing.Font("MS UI Gothic", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.c.Location = new System.Drawing.Point(46, 359);
+            this.c.Name = "c";
+            this.c.Size = new System.Drawing.Size(75, 75);
+            this.c.TabIndex = 16;
+            this.c.Text = "C";
+            this.c.UseVisualStyleBackColor = false;
+            this.c.Click += new System.EventHandler(this.c_Click);
+            // 
+            // equal
+            // 
+            this.equal.BackColor = System.Drawing.Color.SkyBlue;
+            this.equal.Font = new System.Drawing.Font("MS UI Gothic", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.equal.Location = new System.Drawing.Point(433, 359);
+            this.equal.Name = "equal";
+            this.equal.Size = new System.Drawing.Size(75, 75);
+            this.equal.TabIndex = 17;
+            this.equal.Text = "=";
+            this.equal.UseVisualStyleBackColor = false;
+            this.equal.Click += new System.EventHandler(this.equal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 471);
+            this.Controls.Add(this.equal);
+            this.Controls.Add(this.c);
+            this.Controls.Add(this.dot);
+            this.Controls.Add(this.div);
+            this.Controls.Add(this.mul);
+            this.Controls.Add(this.sub);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.n0);
+            this.Controls.Add(this.n9);
+            this.Controls.Add(this.n8);
+            this.Controls.Add(this.n7);
+            this.Controls.Add(this.n6);
+            this.Controls.Add(this.n5);
+            this.Controls.Add(this.n4);
+            this.Controls.Add(this.n3);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.n1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button n1;
+        private System.Windows.Forms.Button n2;
+        private System.Windows.Forms.Button n3;
+        private System.Windows.Forms.Button n4;
+        private System.Windows.Forms.Button n5;
+        private System.Windows.Forms.Button n6;
+        private System.Windows.Forms.Button n7;
+        private System.Windows.Forms.Button n8;
+        private System.Windows.Forms.Button n9;
+        private System.Windows.Forms.Button n0;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button sub;
+        private System.Windows.Forms.Button mul;
+        private System.Windows.Forms.Button div;
+        private System.Windows.Forms.Button dot;
+        private System.Windows.Forms.Button c;
+        private System.Windows.Forms.Button equal;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        double FirstNumber;  // 最初の数値
+        string Operation;   // 演算子
+        bool dotCheck = false;  // "."が押されたフラグ
+        bool errorMsg = false;  // エラーメッセージが出たフラグ
+        bool endCalc = false;   // 演算終了時フラグ
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 表示画面とフラグの初期化
+        public void Init()
+        {
+            textBox1.Text = "0";
+            dotCheck = false;
+            errorMsg = false;
+            endCalc = false;
+        }
+
+        // 1をクリックした時
+        private void n1_Click(object sender, EventArgs e)
+        {
+            // 0かエラーメッセージもしくは演算終了時なら1を表示
+            if (textBox1.Text == "0" || errorMsg || endCalc)
+            {
+                textBox1.Text = "1";
+            }
+            else    // それ以外なら1を右に追加
+            {
+                textBox1.Text = textBox1.Text + "1";
+            }
+        }
+
+        // 2をクリックした時
+        private void n2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" || errorMsg || endCalc)
+            {
+                textBox1.Text = "2";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "2";
+            }
+        }
+
+        // 3をクリックした時
+        private void n3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" || errorMsg || endCalc)
+            {
+                textBox1.Text = "3";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "3";
+            }
+        }
+
+        // 4をクリックした時
+        private void n4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" || errorMsg || endCalc)
+            {
+                textBox1.Text = "4";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "4";
+            }
+        }
+
+        // 5をクリックした時
+        private void n5_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" || errorMsg || endCalc)
+            {
+                textBox1.Text = "5";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "5";
+            }
+        }
+
+        // 6をクリックした時
+        private void n6_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" || errorMsg || endCalc)
+            {
+                textBox1.Text = "6";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "6";
+            }
+        }
+
+        // 7をクリックした時
+        private void n7_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" || errorMsg || endCalc)
+            {
+                textBox1.Text = "7";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "7";
+            }
+        }
+
+        // 8をクリックした時
+        private void n8_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" || errorMsg || endCalc)
+            {
+                textBox1.Text = "8";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "8";
+            }
+        }
+
+        // 9をクリックした時
+        private void n9_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" || errorMsg || endCalc)
+            {
+                textBox1.Text = "9";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "9";
+            }
+        }
+
+        // 0をクリックした時
+        private void n0_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" || errorMsg || endCalc)
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "0";
+            }
+        }
+
+        // +をクリックした時
+        private void add_Click(object sender, EventArgs e)
+        {
+            // 最初の数値をダブル型に変換
+            FirstNumber = Convert.ToDouble(textBox1.Text);
+
+            // 表示画面とフラグの初期化
+            Init();
+
+            // 演算子+を記憶
+            Operation = "+";
+        }
+
+        // -をクリックした時
+        private void sub_Click(object sender, EventArgs e)
+        {
+            FirstNumber = Convert.ToDouble(textBox1.Text);
+            Init();
+            Operation = "-";
+        }
+
+        // *をクリックした時
+        private void mul_Click(object sender, EventArgs e)
+        {
+            FirstNumber = Convert.ToDouble(textBox1.Text);
+            Init();
+            Operation = "*";
+        }
+
+        // /をクリックした時
+        private void div_Click(object sender, EventArgs e)
+        {
+            FirstNumber = Convert.ToDouble(textBox1.Text);
+            Init();
+            Operation = "/";
+        }
+
+        // Cをクリックした時
+        private void c_Click(object sender, EventArgs e)
+        {
+            // 表示画面とフラグの初期化
+            Init();
+        }
+
+        // .をクリックした時
+        private void dot_Click(object sender, EventArgs e)
+        {
+            // すでに"."が押されていないか確認
+            if (!dotCheck)
+            {
+                // テキストボックスの右に"."を表示
+                textBox1.Text = textBox1.Text + ".";
+
+                // "."が押された
+                dotCheck = true;
+            }
+        }
+
+        // =をクリックした時
+        private void equal_Click(object sender, EventArgs e)
+        {
+            // 次の数値
+            double SecondNmber = 0;
+
+            // エラーメッセージが出ていないか確認
+            if (!errorMsg)
+            {
+                // 次の数値をダブル型に変換
+                SecondNmber = Convert.ToDouble(textBox1.Text);
+            }
+
+            switch (Operation)
+            {
+                // 演算子が+なら
+                case "+":
+                    // 合計値を表示
+                    textBox1.Text = Convert.ToString(FirstNumber + SecondNmber);
+
+                    // 演算終了時
+                    endCalc = true;
+                    break;
+
+                // 演算子が-なら
+                case "-":
+                    textBox1.Text = Convert.ToString(FirstNumber - SecondNmber);
+                    endCalc = true;
+                    break;
+
+                // 演算子が*なら
+                case "*":
+                    textBox1.Text = Convert.ToString(FirstNumber * SecondNmber);
+                    endCalc = true;
+                    break;
+
+                // 演算子が/なら
+                case "/":
+                    // 次の数値が0の場合
+                    if (SecondNmber == 0)
+                    {
+                        // エラーメッセージの表示
+                        textBox1.Text = "ゼロで割る事はできません";
+
+                        // エラーメッセージが出た
+                        errorMsg = true;
+
+                        // 演算終了時
+                        endCalc = true;
+                    }
+                    else
+                    {
+                        textBox1.Text = Convert.ToString(FirstNumber / SecondNmber);
+                        endCalc = true;
+                    }
+                    break;
+            }
+
+            // 演算終了時なら最初と次の数値を初期化
+            if (endCalc)
+            {
+                FirstNumber = 0;
+                SecondNmber = 0;
+            }
+        }
+    }
+}
